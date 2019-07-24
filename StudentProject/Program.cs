842cf9a2a9f95e0ea27113f7a0873de4da94d0a8
Titus Moore <titusdmoore@gmail.com>
2019-07-24 15:58:59 -0400

    Added Interesting techniques for usage of classes methods and contructs

--- StudentProject/Program.cs
+++ StudentProject/Program.cs
@@ -7,12 +7,15 @@ namespace StudentProject {
             //Add Brailee as a student
             Student brailee = new Student();
             brailee.firstName = "Brailee";
+            brailee.setStatus("CURRENT");
+            Console.WriteLine($"Brailee's status is: {brailee.status}");
             brailee.isPaid = true;
             brailee.birthdate = new DateTime(1990, 01, 01);
 
             // Add Jesse as a student
             Student jesse = new Student();
             jesse.firstName = "Jesse";
+            jesse.setStatus("past");
             jesse.lastName = "Kyle";
             jesse.isPaid = true;
             jesse.birthdate = new DateTime(1990, 1, 1);
@@ -20,10 +23,11 @@ namespace StudentProject {
             // Add Titus as a student
             Student titus = new Student();
             titus.firstName = "Titus";
+            titus.setCreditScore(500);
             titus.lastName = "Moore";
             titus.isPaid = true;
             titus.birthdate = new DateTime(2001, 04, 09);
-
+            Console.WriteLine($"Titus's credit score is: {titus.creditScore}");
             // Concat Name and Console.WriteLine();
             string name = titus.firstName + " " + titus.lastName;
             Console.WriteLine(name);
@@ -33,7 +37,17 @@ namespace StudentProject {
             john.firstName = "John";
             john.isPaid = false;
             john.state = "California";
+            // Using the datetime constructor
             john.birthdate = new DateTime(1999, 05, 01, 12, 30, 00);
+            john.bootcamp = ".Net";
+            john.status = "future";
+
+            // Adding Alex Chan using the new Student constructor
+            Student alex = new Student("Alex", "Chan");
+
+            // Using some interpolation
+            Console.WriteLine($"Alex: {alex.firstName}, {alex.lastName}");
+
 
         }
         class Student {
@@ -49,8 +63,37 @@ namespace StudentProject {
             public string phone { get; set; }
             public bool isPaid { get; set; } = false;
             public DateTime birthdate { get; set; }
+            public int creditScore { get; private set; }
 
+            // Methods
+            public void setStatus (string statusSet) {
+                if (statusSet == "CURRENT" 
+                    || statusSet == "PAST" 
+                    || statusSet == "FUTURE") {
+                    this.status = statusSet;
+                } else {
+                    Console.WriteLine("Status must be: PAST, CURRENT, or FUTURE");
+                }
+            }
+
+            public void setCreditScore(int scoreCheck) {
+                if (scoreCheck >= 400 && scoreCheck <= 900) {
+                    this.creditScore = scoreCheck;
+                } else {
+                    Console.WriteLine("Credit Score must be between 400 and 900");
+                }
+            }
+
+            // Constructors
             public Student() {
+                //Unless otherwise specified Bootcap will default to .Net, only when using this constructor
+                this.bootcamp = ".Net";
+            }
+            public Student (string firstName, string lastName) {
+                this.firstName = firstName;
+                this.lastName = lastName;
+                //When using this constructor the default Bootcamp is Java
+                this.bootcamp = "Java";
             }
         }
     }
